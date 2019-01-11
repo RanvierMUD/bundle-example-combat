e49486023320894d088fb77aefaed188396dbfb8
Shawn Biddle <shawncplus@gmail.com>
2019-01-10 20:35:28 -0500

    Fix rando require

--- commands/flee.js
+++ commands/flee.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const Random = require('rando-js');
+const { Random } = require('rando-js');
 const { Broadcast } = require('ranvier');
 const { CommandParser } = require('@bundles/bundle-example-lib/lib/CommandParser');
 const say = Broadcast.sayAt;
